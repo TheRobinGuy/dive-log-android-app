95e8d76faea15dbdfaab04615184642385e2bab1
TheRobinGuy <gilmartin.glenn@gmail.com>
2022-10-04 14:29:18 +0100

    Splash screen logic complete

--- app/src/main/java/com/example/divelog/SplashActivity.kt
+++ app/src/main/java/com/example/divelog/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.example.divelog
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        val intent = Intent(this, MainActivity::class.java)
+                Handler().postDelayed({
+                        startActivity(intent)
+                        finish()
+        }, 1000)
     }
 }
